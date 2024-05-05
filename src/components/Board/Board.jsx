d7f0135e513e3a891b38897a1685521518ea4760
Maks <maks.tk95@gmail.com>
2024-05-05 14:05:46 +0200

    I made: draw cards on every refresh page, comparing cards, win msg when every card is guessed

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,50 +1,95 @@
 import { Card } from '../Card/Card'
 import { cardsArr } from '../../utils/cards'
 import styles from './Board.module.scss'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export function Board() {
 	const [cards, setCards] = useState(cardsArr)
+	const [firstSelectedCard, setFirstSelectedCard] = useState(null)
+	const [secondSelectedCard, setSecondSelectedCard] = useState(null)
+	const [isComparing, setIsComparing] = useState(false)
 
-	function handleIsShownCardClick(id) {
+	useEffect(() => {
+		shuffleCardsClick()
+	}, [])
+
+	function handleCardClick(clickedCard) {
+		if (!isComparing) {
+			setCards(
+				cards.map(card => {
+					if (card.id === clickedCard.id) {
+						return { ...card, isShown: true }
+					}
+					return card
+				})
+			)
+			if (!firstSelectedCard) {
+				setFirstSelectedCard(clickedCard)
+			} else {
+				setSecondSelectedCard(clickedCard)
+				setIsComparing(true)
+				if (firstSelectedCard.sign === clickedCard.sign) {
+					setFirstSelectedCard(null)
+					setSecondSelectedCard(null)
+					setIsComparing(false)
+				} else {
+					setTimeout(() => {
+						setCards(
+							cards.map(card => {
+								if (card.id === firstSelectedCard.id || card.id === clickedCard.id) {
+									return { ...card, isShown: false }
+								}
+								return card
+							})
+						)
+						setFirstSelectedCard(null)
+						setSecondSelectedCard(null)
+						setIsComparing(false)
+					}, 1000)
+				}
+			}
+		}
+	}
+
+	function shuffleCardsClick() {
+		const shuffleCards = [...cards].sort(() => Math.random() - 0.5)
+		setCards(shuffleCards)
+		resetCard()
+	}
+	function resetCard() {
 		setCards(prevCards =>
 			prevCards.map(card => {
-				if (card.id === id) {
-					return {
-						...card,
-						isShown: true,
-					}
-				} else {
-					return card
+				return {
+					...card,
+					isShown: false,
 				}
 			})
 		)
 	}
 
-    function shuffleCardsClick() {
-        const shuffleCards = [...cards].sort(() => Math.random() - 0.5)
-        setCards(shuffleCards)
-        resetCard()
-    }
-    function resetCard() {
-        setCards(prevCards => prevCards.map(card => {
-            return {
-                ...card,
-                isShown: false
-            }
-        }))
-    }
+	let winMsg
+	const shownCardsArr = cards.every(card => card.isShown === true)
+	if(shownCardsArr) {
+		winMsg = 'Wygrałeś! 🎉'
+	}
+	/*
+		todo:
+		4) odgadnięte karty pozostają odkryte w momencie kiedy probowałbym je porównywać do nieodgadniętych
+	*/
 
-	const cardsBoard = cards.map(({ id, sign, isShown }) => (
-		<Card key={id} isShown={isShown} onShownClick={() => handleIsShownCardClick(id)}>
-			{isShown ? sign : '❓'}
+	const cardsBoard = cards.map(card => (
+		<Card key={card.id} isShown={card.isShown} onShownClick={() => handleCardClick(card)}>
+			{card.isShown ? card.sign : '❓'}
 		</Card>
 	))
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.board}>{cardsBoard}</div>
-			<button onClick={shuffleCardsClick} className={styles.btnReset}>Zresetuj</button>
+			<h1 className={styles.winMsg}>{winMsg}</h1>
+			<button onClick={shuffleCardsClick} className={styles.btnReset}>
+				Zresetuj
+			</button>
 		</div>
 	)
 }
